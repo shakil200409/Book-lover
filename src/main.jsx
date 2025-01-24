07ef2ee9dc66df4aeb0d09cedd7bc4ff10fba6bf
Shakil Ahmed <shakil200409@gmail.com>
2025-01-24 22:46:12 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Components/Root.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
